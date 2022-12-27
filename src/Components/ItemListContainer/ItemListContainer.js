83447b12ebcc1d94bcf0bee2e44c0c245e28723f
Mariano <marianotrogo@gmail.com>
2022-12-27 11:38:41 -0300

    nuevo

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,28 +1,34 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Item from '../Item/Item'
 
 const ItemListContainer = () => {
-  const [user, setUser] = useState ([
+  const prod = [
     {
       id: 1,
       name: "Fifa 23",
-      precio: "U$D 80",
+      precio: "U$D 59.99",
       img: "https://sm.ign.com/ign_es/screenshot/default/image002-1_3te9.jpg"
     },
     {
       id: 2,
       name: "God of War Ragnarok",
-      precio: "U$D 100",
+      precio: "U$D 69.99",
       img:"https://i.3djuegos.com/juegos/17544/god_of_war_ragnarok/fotos/ficha/god_of_war_ragnarok-5732812.jpg"
     },
-  ]);
+    {
+      id: 3,
+      name: "Shadow of the Colossuss",
+      precio: "U$D 39.99",
+      img:"https://media.playstation.com/is/image/SCEA/shadow-of-the-colossus-box-art-01-ps4-us-25jan18?$native_nt$"
+    },
+  ];
   
 
 
   return (
 
     <div>
-      {user.map((item)=>{
+      {prod.map((item)=>{
         return(
           <Item 
             img={item.img}
