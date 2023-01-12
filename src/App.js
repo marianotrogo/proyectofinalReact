091a70b0054bcf0128997451f1a044430307dc49
Mariano <marianotrogo@gmail.com>
2023-01-12 10:38:32 -0300

    nuevos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/NavBar/Navbar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import Cart from './Components/Cart/Cart';
-
-
-
+import {useState} from 'react';
 
 
 function App() {
+  
+  
   return (
     <>
       <Router>
         <Navbar/>
         <Routes>
-          
+          <Route path='/'/>
           <Route path='/ItemListContainer' element={<ItemListContainer/>}/>
           <Route path='/Cart' element={<Cart/>}/>
         </Routes>
--- src/Components/ItemListContainer/Item.js
+++ src/Components/ItemListContainer/Item.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { prod } from './Productos'
+import './Item.css'
+
 
 const Item = () => {
+  const [product, setProduct] = useState(prod)
+
+  const deleteProduct = (productId) => {
+    const cambioCart = product.filter(product => product.id !== productId);
+    setProduct(cambioCart);
+  }
+  const addCart = (addProduct) => {
+    const cambioCart = [
+      addProduct.id
+
+    ];
+    setProduct(cambioCart);
+  }
+
   return (
-    <div>Item</div>
+    <>
+      {product.map(product => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.descripcion}</p>
+          <p>Categoria: {product.category}</p>
+          <img className='imgProd' src={product.img} />
+
+          <button>Agregar al Carrito</button>
+          <button onClick={() => deleteProduct(product.id)}>Eliminar</button>
+        </div>
+      ))}
+
+    </>
   )
 }
 
