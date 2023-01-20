9aea36ef644ff9131ff9428f960c6f882331ce5f
Mariano <marianotrogo@gmail.com>
2023-01-20 18:34:07 -0300

    otrog

--- src/Components/ItemDetailContainer/ItemDetail.js
+++ src/Components/ItemDetailContainer/ItemDetail.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ItemCount from './ItemCount'
 
 
 const ItemDetail = ({ data }) => {
+
+  const [irAlCarrito, setIrAlCarrito] = useState(second)
+
+  const onAdd = (quantity) =>{
+    
+  }
+
   return (
     <>
       <div>
@@ -10,6 +18,7 @@ const ItemDetail = ({ data }) => {
           <div>
             <h2>{data.name}</h2>
           </div>
+          <ItemCount initial = {0} stock = {5} onAdd={onAdd}/>
         </div>
       </div>
     </>
